232d8714faec57124b5976acaf7c98882e22e0dc
김민회 <70158840+mine7272@users.noreply.github.com>
2024-01-31 13:31:57 +0900

    Title: 지그재그로 이동하기, Time: 255ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/지그재그로 이동하기/moving-with-zigzag.py
+++ 240131/지그재그로 이동하기/moving-with-zigzag.py
@@ -4,15 +4,28 @@ swi = 0
 res = 0
 now = a
 
-while (now < b) :
-    if swi == 0 :
-        res += abs(now - (a+i))
-        now = a+i
-        swi = 1
-    elif swi == 1 :
-        res += abs(now - (a-i))
-        now = a-i
-        swi = 0
-    i *=2
+if b > a : 
+    while (now < b) :
+        if swi == 0 :
+            res += abs(now - (a+i))
+            now = a+i
+            swi = 1
+        elif swi == 1 :
+            res += abs(now - (a-i))
+            now = a-i
+            swi = 0
+        i *=2
+else : 
+    while (now > b) :
+        if swi == 0 :
+            res += abs(now - (a+i))
+            now = a+i
+            swi = 1
+        elif swi == 1 :
+            res += abs(now - (a-i))
+            now = a-i
+            swi = 0
+        i *=2
+
 res = res - (now - b)
 print(res)
