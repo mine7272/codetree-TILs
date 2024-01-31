801a66a92e44a20fe598dbe23bf15f668f041b72
김민회 <70158840+mine7272@users.noreply.github.com>
2024-01-31 09:27:34 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240131/두 사람/two-person.py
+++ 240131/두 사람/two-person.py
@@ -2,4 +2,4 @@ age, gender = input().split()
 age2, gender2 = input().split()
 age, age2=int(age), int(age2)
 
-print(1 if ((age or age2) >= 19) and ((gender or gender2) == "M") else 0)
+print (1 if ((age >= 19 and gender == "M") or (age2 >=19 and gender2 = "M")) else 0)
