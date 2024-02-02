f45702064d2d76259aae618eb4d95582a3cd979e
김민회 <70158840+mine7272@users.noreply.github.com>
2024-02-02 15:37:35 +0900

    Title: 체크 다이아몬드, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240202/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(1, n+1):
+    for _ in range(n-i, 0, -1):
+        print(" ", end = "")
+    for _ in range(i):
+        print("*", end = " ")
+    print()
+
+for i in range(n-1, 0, -1):
+    for _ in range(n-i, 0, -1):
+        print(" ", end = "")
+    for _ in range(i):
+        print("*", end = " ")
+    print()
