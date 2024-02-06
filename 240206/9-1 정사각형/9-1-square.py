539314b999b9e665c7d72ec38350aa59f2599846
김민회 <70158840+mine7272@users.noreply.github.com>
2024-02-06 13:27:21 +0900

    Title: 9-1 정사각형, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240206/9-1 정사각형/9-1-square.py
@@ -0,0 +1,10 @@
+n = int(input())
+cnt = 9
+
+for i in range (1,n+1) :
+    for j in range (1, n+1):
+        print(cnt,end='')
+        cnt -= 1
+        if cnt == 0 :
+            cnt = 9
+    print("")
